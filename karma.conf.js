659976ee58f3db5fd42c12b0b79df008a7661a24
Alexey Ekimov <AEkimov2@slb.com>
2020-04-10 12:22:22 -0500

    update karma.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,14 +1,21 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const os = require('os');
+
 module.exports = function (config) {
+
+  // Run headless from Linux
+  let defaultBrowser = os.platform() === 'linux' ? 'ChromeHeadless' : 'Chrome';
+
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine',  '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -23,17 +30,48 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/dls-boilerplate-material'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      dir: require('path').join(__dirname, '../test-reports/app'),
+      reports: ['html', 'lcovonly', "cobertura", 'text-summary'],
+      fixWebpackSourcePaths: true,
+      "report-config": {
+          cobertura: {
+              file: "cobertura.xml",
+          },
+          lcovonly: {
+              file: "coverage.lcov"
+          },
+      }
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: '../test-reports/app'
+    },
+    reporters: config.angularCli && config.angularCli.codeCoverage
+      ? ['progress', 'junit', 'coverage-istanbul']
+      : ['progress', 'junit', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [defaultBrowser],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      },
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          // See https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+          '--remote-debugging-address=0.0.0.0'
+        ]
+      }
+    }
   });
 };
